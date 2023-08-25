22f3b1f82087a6363e2ac49f5de2d891ccbb42ed
Felix Brüggemann <Felix.brgm@icloud.com>
2023-08-25 15:30:38 +0200

    f

--- setup.sh
+++ setup.sh
@@ -2,5 +2,23 @@
 # For setting up my unix development setup
 
 # Create symlinks
-rm ~/.bashrc
-ln -s "$(pwd)/.bashrc" ~/.bashrc
+# ln -s "$(pwd)/.bashrc" ~/.bashrc
+
+
+for file in ./* .[^.]*; do
+    filename=$(basename "$file")
+    
+    if [ "$filename" != "setup.sh" ]; then
+        if [ "$filename" != ".git" ]; then
+            target_path="~/$filename"
+        
+            # Check if the target file already exists (whether it's a file or symlink)
+            if [ -e "$target_path" ]; then
+                rm $filename
+            else
+                ln -s "$(pwd)/$file" "$target_path"
+            fi
+        fi
+    fi
+    echo $filename
+done
