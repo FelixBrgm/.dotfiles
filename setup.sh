902fa250d652e49fdce3a60b7debc2bddb67ae48
Felix Brüggemann <Felix.brgm@icloud.com>
2023-08-25 14:59:42 +0200

    manual linking

--- setup.sh
+++ setup.sh
@@ -2,23 +2,5 @@
 # For setting up my unix development setup
 
 # Create symlinks
-
-# Source directory containing the files
-source_directory="."
-
-# Target directory where symlinks will be created
-target_directory="~/"
-
-for file in "$source_directory"/*; do
-    filename=$(basename "$file")
-    
-    if [ "$filename" != "setup.sh" ]; then
-        target_path="$target_directory/$filename"
-        
-        # Check if the target file already exists (whether it's a file or symlink)
-        if [ -e "$target_path" ]; then
-            rm "$target_path"
-        fi
-        ln -s "$file" "$target_path"
-    fi
-done
+rm ~/.bashrc
+ln -s .bashrc ~/.bashrc
